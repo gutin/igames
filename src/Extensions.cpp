aea0f013cbb693f924a83495133e2cbb90a328d7
Eli Gutin <gutineli@gmail.com>
2013-10-06 09:47:39 +0100

    Working on crashing game

--- src/Extensions.cpp
+++ src/Extensions.cpp
@@ -1,5 +1,6 @@
 #include "Extensions.hpp"
 #include <cmath>
+#include <limits>
 
 namespace ig { namespace core {
 
@@ -49,6 +50,75 @@ double ImplementationUncertaintyEvaluator::evaluate(const UDC& udc_,
     value += udc_.value(nextTau) * outcomeProb;
   }
   return value;
+}
+
+double CrashingEvaluator::evaluate(const UDC& udc_,
+                                  const UDCNetwork& unet_,
+                                  uvertex_t uv_,
+                                  const Network& net_, 
+                                  const State& state_,
+                                  const ActionSharedPtr& candidate_,
+                                  double totalRate_, size_t budget_,
+                                  size_t fcode_, StateTemplateMap& stmap_)
+{
+  if(state_._active.find(net_.end()) != state_._active.end() ||
+     state_._dormant.find(net_.end()) != state_._dormant.end())
+  {
+    return 0;
+  }
+  double minValue = std::numeric_limits<double>::max();
+  BOOST_FOREACH(vertex_t t, state_._active)
+  {
+    double enu = 1, denom = 0;
+    double crashingAmount = (unet_._maxParallel - state_._res) / TASK_ATTR(t, _crashingCost);  
+
+    BOOST_FOREACH(vertex_t u, state_._active)
+    {
+      double nextVal = 0;
+      StateTemplate& nextST = nextTemplate(u, udc_, uv_, stmap_, net_, unet_, fcode_);
+      const UDC& stUDC = unet_[nextST._udc]; 
+      if(stUDC._taskSet.find(net_.end()) != stUDC._taskSet.end())
+      {
+        nextVal = 0;
+      }
+      else
+      {
+        size_t tav = (1L << (2* stUDC._tasks.size())) *
+                   (budget_ - state_._res + candidate_->size());
+        tav |= (1L << stUDC._tasks.size()) * nextST._activationCode;
+        size_t tmp = (~0) - ((1L << stUDC._tasks.size()) - 1L) + nextST._activationCode;
+        tav |= ~(tmp);
+
+        BOOST_FOREACH(vertex_t t, *candidate_)
+        {
+          if(stUDC._taskSet.find(t) != stUDC._taskSet.end())
+          {
+            tav &= ~(1L << stUDC._activity2UDCIndex[t]);
+          }
+        }
+        BOOST_FOREACH(vertex_t t, state_._interdicted)
+        {
+          if(stUDC._taskSet.find(t) != stUDC._taskSet.end())
+          {
+            tav &= ~(1L << stUDC._activity2UDCIndex[t]);
+          }
+        }
+        if(stUDC._taskSet.find(u) != stUDC._taskSet.end())
+        {
+          tav &= ~(1L << stUDC._activity2UDCIndex[u]);
+        }
+        nextVal = stUDC.value(tav);
+      }
+      double xi = t == u ? crashingAmount : 1;
+      double rate = util::rate(u, net_, state_, candidate_);
+      enu += nextVal * rate * xi;
+      denom += rate * xi; 
+    }
+
+    double val = enu / denom;
+    minValue = std::min(minValue, val);
+  }
+  return minValue;
 }
 
 }}
--- src/Task.hpp
+++ src/Task.hpp
@@ -5,11 +5,26 @@ namespace ig { namespace core {
 
 struct Task
 {
+  /*
+   * Expected normal and delayed durations (assuming exponential distros)
+   */
   double expNormal() const { return 1.0 / _nu; }
   double expDelayed() const { return 1.0 / _delta; }
 
+  /*
+   * Normal and delayed rates
+   */
   double _nu, _delta;
+
+  /*
+   *Probability interdiction is successful
+   */
   double _probDelaySuccess;
+
+  /*
+   * Unit cost of crashing that task by the single renewable resource
+   */
+  double _crashingCost;
 };
 
 }}
